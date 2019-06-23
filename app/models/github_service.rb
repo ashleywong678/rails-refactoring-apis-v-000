fe4b55f1f00bfee9bc7baac807d3cf183ec41ec0
Ashley <ashleywong678@gmail.com>
2019-06-23 16:31:36 -0400

    Done.

--- app/models/github_service.rb
+++ app/models/github_service.rb
@@ -6,8 +6,14 @@ class GithubService
     self.access_token = options['access_token']
   end
 
-  def authenticate!
+  def authenticate!(client, secret, code)
+    resp = Faraday.post "https://github.com/login/oauth/access_token", {client_id: client, client_secret: secret,code: code}, {'Accept' => 'application/json'}
+    access_hash = JSON.parse(resp.body)
+    self.access_token = access_hash["access_token"]
+  end
+
+  def get_username
 
   end
-  
+
 end
